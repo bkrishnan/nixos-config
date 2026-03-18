b5381aaecbaca3e185787e2f74572ee9917f1018
Bharath Krishnan <bkrishnan@gmail.com>
2026-03-18 18:11:51 -0400

    Add SSH alias for connecting to seekingvisa.com

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -11,6 +11,7 @@
       nrt = "sudo nixos-rebuild test --flake ~/nixos-config#imac";
       slog = "journalctl -u sanoid -u sanoid-prune.service -u syncoid -f";
       smon = "sudo sanoid --monitor-snapshots | tr , '\n'";
+      aseek = "ssh -i ~/.ssh/seeking-visa.pem ubuntu@www.seekingvisa.com";
     };
 
     functions = {
